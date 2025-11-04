e2986b847f0726f2b99d61fef3166ce503c8c214
Deon Kouatchou <ddkouatchou@gmail.com>
2025-11-04 17:44:08 -0500

    Add computer vision functions to get foliage images and health message in email behavior

--- email_behavior.py
+++ email_behavior.py
@@ -4,10 +4,13 @@ from greenhouse_behaviors import Greenhouse_Behavior
 import send_email
 import datetime
 
+from computer_vision import classify, measure, vision, color_correct, cv_utils
 
 class Email(Greenhouse_Behavior):
     def __init__(self, agent):
         super(Email, self).__init__(agent, "EmailBehavior")
+
+        self.IMAGE_DIRECTORY = "/home/robotanist/User/images"
 
         # BEGIN STUDENT CODE
         self.initial = 'Halt'
@@ -27,6 +30,20 @@ class Email(Greenhouse_Behavior):
         self.fsm.add_transition('disable', self.sending_email, self.initial)
         self.fsm.add_transition('disable', self.on, self.initial)
         # END STUDENT CODE
+
+        # CV-related Initializations
+        stick_mask_path = "./computer_vision/masks/stick_mask_A.jpg"
+        ref_img_path = "./computer_vision/images/measure_ref_image_A.jpg"
+        self.foliage_model = "./computer_vision/foliage_classifier.pkl"
+        self.calib_model = "./computer_vision/calib_classifier.onnx"
+
+        self.ref_img = cv_utils.readImage(ref_img_path)
+        self.stick_mask = cv_utils.readMask(stick_mask_path)
+
+        self.classifier = classify.FoliageClassifier(self.foliage_model)
+        self.measurer = measure.MeasureHeight(self.ref_img, self.stick_mask)
+
+        self.greenery = self.height = 0
 
     def parse_sensor_data(self):
         sensors = self.sensors
@@ -57,12 +74,59 @@ class Email(Greenhouse_Behavior):
         <p>LED: {"On" if actuator_state["led"] else "Off"}</p>
         """
         return actuator_data
+    
+    def get_most_recent_image(self):
+        # Gets most recent image from saved images from camera behavior
+        import os
+
+        img_paths = []
+        for entry in os.listdir(self.img_dir):
+            path = os.path.join(self.img_dir, entry)
+            if os.path.isfile(path):
+                img_paths.append(path)
+        if len(img_paths) == 0:
+            return None
+        else:
+            img_cdates = list(map(lambda p: os.path.getctime(p), img_paths))
+            target_img_path = img_paths[img_cdates.index(max(img_cdates))]
+            return target_img_path
+    
+    def get_foliage_images(self):
+        # Perform color calibration on target image based on reference image
+        target_img_path = self.get_most_recent_image()
+        target_img = cv_utils.readImage(target_img_path)
+
+        corrector = color_correct.ColorCorrector(self.ref_img)
+        corrector.findRegion(self.calib_model)
+        corrected_image = corrector.correct(target_img)
+
+        # Get foliage images
+        foliage_image, height_image, height = vision.foliageImages(corrected_image, self.classifer, self.measurer)
+        return foliage_image, height_image
+
+
+    def get_plant_health_assessment(self):
+        # Perform color calibration on target image based on reference image
+        target_img_path = self.get_most_recent_image()
+        target_img = cv_utils.readImage(target_img_path)
+
+        corrector = color_correct.ColorCorrector(self.ref_img)
+        corrector.findRegion(self.calib_model)
+        corrected_image = corrector.correct(target_img)
+
+        # Estimate plant health
+        self.greenery, self.plant_height, health_msg = vision.plantHealth(
+            corrected_image, self.classifier, self.measurer, self.greenery, self.plant_height)
+        self.plant_height = 0 if self.plant_height is None else self.plant_height
+
+        return health_msg
 
     def create_email(self):
         import os
         import datetime
 
-        IMAGE_DIRECTORY = "/home/robotanist/User/images"
+        # IMAGE_DIRECTORY = "/home/robotanist/User/images"
+        IMAGE_DIRECTORY = self.IMAGE_DIRECTORY
 
         TEAM_NAME = "Team Wendy's Baconhater"
         timestamp = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
